5ea412d9abbc84d76412420f39850607eee08577
Denis <denisv.daros@gmail.com>
2022-04-16 21:05:12 -0300

    oitavo requisito concluido

--- src/challenges.js
+++ src/challenges.js
@@ -49,13 +49,35 @@ function catAndMouse(mouse,cat1,cat2) {
      return "os gatos trombam e o rato foge";
   }
 }
-console.log(catAndMouse(1,0,2))
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
 
+// Crie uma função chamada fizzBuzz que receba uma array de números e retorne uma array da seguinte forma:
+
+// Para cada número do Array que seja divisível apenas por 3, apresente uma string "fizz";
+
+// Para cada número do Array que seja divisível apenas por 5, apresente uma string "buzz";
+
+// Caso o número seja divisível por 3 e 5, retorne a string "fizzBuzz";
+
+// Caso o número não possa ser dividido por 3 nem por 5, retorne a string "bug!";
+// Exemplo: caso o parâmetro seja [2, 15, 7, 9, 45], sua função deverá retornar ["bug!", "fizzBuzz", "bug!", "fizz", "fizzBuzz"].
+
+function fizzBuzz(array) {
+  let arrayStrings =[]
+  for(let index = 0;index < array.length; index +=1) {
+    if(array[index] % 3 === 0 && array[index] % 5 === 0){
+      arrayStrings.push("fizzBuzz")
+    } else if (array[index] % 5 === 0){
+      arrayStrings.push("buzz")
+    } else if (array[index] % 3 === 0){
+      arrayStrings.push("fizz")
+    } else {
+      arrayStrings.push("bug!")
+    }
+  }
+  return arrayStrings
+}
 // Desafio 9
 function encode() {
   // seu código aqui
